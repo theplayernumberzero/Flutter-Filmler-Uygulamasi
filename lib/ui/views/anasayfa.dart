70f03ca641d4cd057afae869fd9a0c1e87b599e5
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-07-08 23:23:36 +0300

    uygyulama temeli

---
+++ lib/ui/views/anasayfa.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Filmler"),
+      ),
+      body: const Center(),
+    );
+  }
+}
---
+++ lib/ui/views/detay_sayfa.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetaySayfa extends StatefulWidget {
+  const DetaySayfa({super.key});
+
+  @override
+  State<DetaySayfa> createState() => _DetaySayfaState();
+}
+
+class _DetaySayfaState extends State<DetaySayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Filmler Detay"),
+      ),
+      body: const Center(),
+    );
+  }
+}
